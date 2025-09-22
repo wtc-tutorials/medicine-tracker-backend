9eed784f68fc47ed645c4cabac26de4786d9f072
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2025-09-22 03:35:57 +0200

    update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,12 @@
 # database.py
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+DATABASE_URL = "postgresql+asyncpg://user:password@localhost:5432/medtracker"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
