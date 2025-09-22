0096a4ef83b0fee6468143a7da47ea97f21b332e
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2025-09-22 15:05:05 +0200

    update app/main.py to use async table creation

--- app/main.py
+++ app/main.py
@@ -1,14 +1,21 @@
-# main.py
+# backend/app/main.py
 from fastapi import FastAPI
 from .routers import medicines
-from .database import Base, engine
-from . import models
-
-
-# ✅ Create tables in the connected DB
-Base.metadata.create_all(bind=engine)
+from .database import engine, Base
 
 app = FastAPI(title="Medicine Tracker API")
+
+
+# ✅ async table creation
+async def init_models():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+# run init once at startup
+@app.on_event("startup")
+async def on_startup():
+    await init_models()
 
 
 app.include_router(medicines.router, prefix="/medicines", tags=["medicines"])
