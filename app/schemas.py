fbf96b3495a2b209e50fa7ebbe357d97e8632c0a
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2025-09-22 03:41:51 +0200

    update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,1 +1,24 @@
 # schemas.py
+from pydantic import BaseModel
+from datetime import date
+from typing import Optional
+
+
+class MedicineBase(BaseModel):
+    name: str
+    start_date: date
+    total_quantity: int
+    daily_dosage: int
+
+
+class MedicineCreate(MedicineBase):
+    pass
+
+
+class Medicine(MedicineBase):
+    id: int
+    end_date: date
+    remaining: int
+
+    class Config:
+        orm_mode = True
