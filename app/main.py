532f4517c10a9776edec233e79a949a2b25185b1
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2025-09-22 06:18:27 +0200

    update app/main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 # main.py
 from fastapi import FastAPI
 from .routers import medicines
+from .database import Base, engine
+from . import models
+
+
+# ✅ Create tables in the connected DB
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Medicine Tracker API")
 
+
 app.include_router(medicines.router, prefix="/medicines", tags=["medicines"])
+
+
+@app.get("/")
+def root():
+    return {"message": "Medicine Tracker API running"}
