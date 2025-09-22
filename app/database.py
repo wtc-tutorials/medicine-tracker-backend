8595f396073ead89a80b0865e7e49588ebefda59
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2025-09-22 06:06:59 +0200

    update database.py to read credentials from dotenv

--- app/database.py
+++ app/database.py
@@ -1,8 +1,21 @@
 # database.py
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql+asyncpg://user:password@localhost:5432/medtracker"
+load_dotenv()
+
+
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+host = os.getenv("POSTGRES_HOST")
+port = os.getenv("POSTGRES_PORT")
+database = os.getenv("POSTGRES_DB")
+
+DATABASE_URL = f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{database}"
+
+print(f"\n{DATABASE_URL = }")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
