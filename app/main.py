e19d84bd9389661b90b576ec58862474d67f0b7d
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2025-09-22 05:39:47 +0200

    update app/main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
 # main.py
+from fastapi import FastAPI
+from .routers import medicines
+
+app = FastAPI(title="Medicine Tracker API")
+
+app.include_router(medicines.router, prefix="/medicines", tags=["medicines"])
